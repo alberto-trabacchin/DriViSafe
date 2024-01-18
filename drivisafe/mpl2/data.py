1327c71b03ab2b6598ed9da1db7cef42819dc67a
Alberto Trabacchin <alberto.trabacchin@gmail.com>
2024-01-18 16:42:26 +0900

    Updated Dataset structure for MPL.

--- drivisafe/mpl2/data.py
+++ drivisafe/mpl2/data.py
@@ -6,8 +6,16 @@ from PIL import Image
 from torchvision import datasets
 from torchvision import transforms
 from torchvision.transforms import InterpolationMode
+from typing import List, Tuple
+from torch.utils.data import Dataset
+from pathlib import Path
+from data_setup import DreyeveDataset
+import json
+from matplotlib import pyplot as plt
 
 from augmentation import RandAugmentCIFAR
+import argparse
+import random
 
 logger = logging.getLogger(__name__)
 
@@ -121,15 +129,60 @@ def get_cifar100(args):
     return train_labeled_dataset, train_unlabeled_dataset, test_dataset, finetune_dataset
 
 
+def get_dreyeve(args):
+    transform_labeled = transforms.Compose([
+        transforms.ToTensor()
+    ])
+    transform_finetune = transforms.Compose([
+        transforms.ToTensor()
+    ])
+    transform_val = transforms.Compose([
+        transforms.ToTensor()
+    ])
+
+    base_dataset = DreyeveSSL(args.data_path, indexs = range(30000))
+    train_labeled_idxs, train_unlabeled_idxs, finetune_idxs = x_u_split(args, base_dataset.targets)
+    print("train_labeled_idxs: ", train_labeled_idxs)
+    
+
+
+def x_u_split_dreyeve(args, targets):
+    train_lb_per_class = args.num_lb_train // args.num_classes
+    val_per_class = args.num_val // args.num_classes
+    test_per_class = args.num_test // args.num_classes
+    labels = np.array(labels)
+
+    train_lb_idxs = []
+    train_ulb_idxs = []
+    val_idxs = []
+    test_idxs = []
+    # TO FIX
+        # for idx, label in enumerate(labels):
+        #     if label in [0, 1]:
+        #         if len(a) < args.num_labeled // args.num_classes:
+        #             a.append(idx)
+        #         else:
+        #             b.append(idx)
+        #     elif label == -1:
+        #         if len(c) < args.num_labeled // args.num_classes:
+        #             c.append(idx)
+        #         else:
+        #             d.append(idx)
+
+
 def x_u_split(args, labels):
     label_per_class = args.num_labeled // args.num_classes
     labels = np.array(labels)
     labeled_idx = []
     # unlabeled data: all training data
     unlabeled_idx = np.array(range(len(labels)))
+    # iterate through all classes
     for i in range(args.num_classes):
+        # get np array of indices of class i
         idx = np.where(labels == i)[0]
+        # take label_per_class random indices from class i
         idx = np.random.choice(idx, label_per_class, False)
+        # extend the list of indices of labeled data
         labeled_idx.extend(idx)
     labeled_idx = np.array(labeled_idx)
     assert len(labeled_idx) == args.num_labeled
@@ -248,7 +301,101 @@ class CIFAR100SSL(datasets.CIFAR100):
             target = self.target_transform(target)
 
         return img, target
+    
+
+class DreyeveSSL(Dataset):
+    """
+    A custom Dataset class for the Dreyeve dataset.
+
+    This class inherits from the PyTorch Dataset class and overrides the __init__ and __getitem__ methods.
+    It represents a dataset of images with optional labels and transformations.
+
+    Attributes:
+        data (List[Path]): A list of paths to the images in the dataset.
+        labels (List[int], optional): A list of labels corresponding to the images. Defaults to None.
+        transform (transforms, optional): A torchvision.transforms instance containing the transformations to be applied to the images. Defaults to None.
+        labels_to_idx (Dict[str, int], optional): A dictionary mapping labels to indices. Defaults to None.
+    """
+    # First annotation: (27489, 01_4720.jpg)
+    def __init__(
+            self,
+            root: str,
+            transform: transforms = None,
+            target_transform: transforms = None,
+            indexs: List[int] = None,
+            mode: str = None,
+            args = None
+    ) -> None:
+        root = Path(root)
+        self.imgs_path = root / "dreyeve"
+        self.annot_path = root / "data_annotations.json"
+        self.root = root
+        self.full_img_names = [f.absolute() for f in self.imgs_path.iterdir() if f.is_file()]
+        img_names = [f.name for f in self.full_img_names]
+        self.targets = [-1] * len(img_names)
+
+        annots = json.load(self.annot_path.open(mode = "r"))
+        self.labels_to_idx = {"Safe": 0, "Dangerous": 1}
+        self.idx_to_labels = {v: k for k, v in self.labels_to_idx.items()}
+        annot_targets = [self.labels_to_idx[d["choice"]] for d in annots]
+        target_fnames = [d["image"][0].split("/")[-1] for d in annots]
+        for annot_id, annot_fname in enumerate(target_fnames):
+            i = img_names.index(annot_fname)
+            target = annot_targets[annot_id]
+            self.targets[i] = target
+
+        if indexs is not None:
+            self.full_img_names = [self.full_img_names[i] for i in indexs]
+            self.targets = [self.targets[i] for i in indexs]
+
+        self.data = []
+        for path in self.full_img_names:
+            self.data.append(Image.open(path))
+        self.data = np.stack(self.data, axis = 0)
+        self.transform = transform
+        self.target_transform = target_transform
+    
+
+    def __getitem__(self, index: int) -> Tuple[str, int]:
+        image = self.data[index]
+        target = self.targets[index]
+
+        if self.transform is not None:
+            image = self.transform(image)
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+        
+        return image, target
+
+
+    def __len__(self) -> int:
+        return len(self.data)
+    
+    def plot(self, index: int) -> None:
+        image = self.data[index]
+        target = self.targets[index]
+        fig, ax = plt.subplots()
+        if target in self.idx_to_labels:
+            ax.set_title(self.idx_to_labels[target])
+        else:
+            ax.set_title("Unknown")
+        ax.imshow(image)
+
+
 
 
 DATASET_GETTERS = {'cifar10': get_cifar10,
                    'cifar100': get_cifar100}
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='MPL Implementation')
+    parser.add_argument("--data_path", type=str, default="./data")
+    parser.add_argument("--num_labeled", type=int, default=2)
+    parser.add_argument("--num_classes", type=int, default=2)
+    parser.add_argument("--seed", type=int, default=0)
+
+    args = parser.parse_args()
+
+    get_dreyeve(args)
+
