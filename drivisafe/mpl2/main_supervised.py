42b3c60bc84e05099c88f9ed7c16ff01c99d13c1
Alberto Trabacchin <alberto.trabacchin@gmail.com>
2024-01-26 19:35:57 +0900

    Update supervised code.

--- drivisafe/mpl2/main_supervised.py
+++ drivisafe/mpl2/main_supervised.py
@@ -9,8 +9,13 @@ import logging
 from vit_pytorch import SimpleViT
 from tqdm import tqdm
 import torch
+import numpy as np
+import random
+import wandb
+import statistics
 
 parser = argparse.ArgumentParser()
+parser.add_argument('--name', default='SimpleViT', type=str, help='experiment name')
 parser.add_argument('--data-path', default='./data', type=str, help='data path')
 parser.add_argument('--dataset', type=str, default='dreyeve', help='dataset name')
 parser.add_argument('--model', type=str, default='simplevit', help='model name')
@@ -26,69 +31,102 @@ parser.add_argument("--num_classes", type=int, default=2, help="number of classe
 parser.add_argument("--num_train_lb", type=int, default=2)
 parser.add_argument("--num_val", type=int, default=2)
 parser.add_argument("--num_test", type=int, default=-1) # <-- Old: now not choosing test size. Just taking all remaining labeled samples.
+parser.add_argument("--seed", type=int, default=42, help="random seed")
 
 args = parser.parse_args()
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
+def set_seeds(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+
+
 def evaluate(model, test_loader, criterion, args):
     accuracies = []
+    losses = []
     test_iter = tqdm(test_loader)
     with torch.inference_mode():
         for step, (images, targets) in enumerate(test_iter):
             batch_size = images.shape[0]
             images = images.to(args.device)
             targets = targets.to(args.device)
-            with autocast(enabled = args.amp):
+            with autocast(enabled = args.amp, device_type="cuda"):
                 logits = model(images)
                 loss = criterion(logits, targets)
             acc1 = (logits.argmax(dim=-1) == targets).float().sum() / batch_size
-            accuracies.append(acc1)
+            accuracies.append(acc1.item())
+            losses.append(loss.item())
             test_iter.set_description(
                 f"Test Iter: {step+1:3}/{len(test_loader):3}."
-                f"Test Acc: {acc1:.4f}. Test Loss: {loss.item():.4f}."
             )
-    mean_acc = torch.mean(torch.stack(accuracies)).item()
-    print(f"Mean Acc: {mean_acc:.4f}")
+    
+    mean_acc = statistics.mean(accuracies)
+    mean_loss = statistics.mean(losses)
     test_iter.close()
-    return loss.item(), mean_acc
+    
+    return mean_loss, mean_acc
+
 
 def train_loop(model, lb_loader, val_loader, test_loader, finetune_loader, optimizer, criterion, args):
     logger.info("***** Running Training *****")
     logger.info(f"   Task = {args.dataset}@{args.num_train_lb}")
     logger.info(f"   Total steps = {args.total_steps}")
+    lb_iter = iter(lb_loader)
+    train_losses = []
+    train_accuracies = []
+    top1_acc = 0.0
     for step in range(args.total_steps):
         if step % args.eval_step == 0:
             pbar = tqdm(range(args.eval_step))
         model.train()
         try:
-            images, targets = next(lb_loader)
+            images, targets = next(lb_iter)
         except:
-            print("Reloading lb_loader")
-            lb_loader = iter(lb_loader)
-            images, targets = next(lb_loader)
+            lb_iter = iter(lb_loader)
+            images, targets = next(lb_iter)
         
         images = images.to(args.device)
         targets = targets.to(args.device)
         logits = model(images)
         loss = criterion(logits, targets)
+        train_losses.append(loss.item())
+        train_acc = (logits.argmax(dim=-1) == targets).float().sum() / args.batch_size
+        train_accuracies.append(train_acc.item())
+        train_losses.append(loss.item())
+        optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        optimizer.zero_grad()
         
         pbar.set_description(
             f"Train Iter: {step+1:3}/{args.total_steps:3}."
-            f" Train Loss: {loss.item():.4f}."
+            f" train/loss: {loss.item():.6f}."
         )
         pbar.update()
 
         if (step + 1) % args.eval_step == 0:
+            train_loss = train_losses[0]
+            train_acc = train_accuracies[0]
             pbar.close()
-            test_loss, top1 = evaluate(model, test_loader, criterion, args)
+            test_loss, test_acc = evaluate(model, test_loader, criterion, args)
+            if test_acc > top1_acc:
+                top1_acc = test_acc
+                # save checkpoint
+            print(f"test/loss: {test_loss:.4f}, test/acc: {test_acc:.4f}.")
+            wandb.log({"train/loss": train_loss, 
+                       "train/top1-acc": train_acc,
+                       "test/loss": test_loss,
+                       "test/acc": test_acc})
+            train_losses = []
+            train_accuracies = []
 
 
 if __name__ == "__main__":
+    set_seeds(args.seed)
     lb_dataset, _, val_dataset, test_dataset, finetune_dataset = DATASET_GETTERS[args.dataset](args)
     lb_loader = DataLoader(
         lb_dataset,
@@ -130,7 +168,8 @@ if __name__ == "__main__":
                 heads = 16,
                 mlp_dim = 2048
             )
-
+    model = model.to(args.device)
     optimizer = Adam(model.parameters(), lr=1e-4)
     criterion = CrossEntropyLoss()
+    wandb.init(name=f"{args.name}-{len(lb_dataset)}LB-{len(test_dataset)}TS", project='MPL-DriViSafe', config=args)
     train_loop(model, lb_loader, val_loader, test_loader, finetune_loader, optimizer, criterion, args)
