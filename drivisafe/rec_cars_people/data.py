717b6d19b7ebfd603fa26478b99cb05715ecabba
Alberto Trabacchin <alberto.trabacchin@gmail.com>
2024-02-11 18:14:30 +0900

    Updated code to preprocess dataset.

--- drivisafe/rec_cars_people/data.py
+++ drivisafe/rec_cars_people/data.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from pycocotools.coco import COCO
 import random
 import cv2
+import shutil
 import argparse
 from tqdm import tqdm
 
@@ -20,38 +21,44 @@ def load_data(args):
     return coco
 
 
-def filter_images(dataset, min_size, categories):
-    filt_image_ids = {}
-    cat_ids = dataset.getCatIds(catNms=categories)
-    image_ids = dataset.getImgIds(catIds=cat_ids)
-    assert(len(image_ids) == len(set(image_ids)))
+def filter_annotations(dataset, min_size, categories):
 
-    for img_id in image_ids:
-        ann_ids = dataset.getAnnIds(imgIds=img_id)
-        anns = dataset.loadAnns(ann_ids)
-        for ann in anns:
-            if ann['category_id'] not in cat_ids:
-                continue
+    flt_annots= {}
 
-            bbox = ann['bbox']
-            cat_idx = cat_ids.index(ann['category_id'])
-            cat = categories[cat_idx]
-            
-            if (bbox[2] >= min_size) and (bbox[3] >= min_size):
-                if cat not in filt_image_ids:
-                    filt_image_ids[cat] = []
-                filt_image_ids[cat].append({
-                    'image_id': img_id,
-                    'bbox': bbox
+    for cat in categories:
+        flt_annots[cat] = []
+        cat_id = dataset.getCatIds(catNms=cat)[0]
+        image_ids = dataset.getImgIds(catIds=cat_id)
+        annots = dataset.loadAnns(dataset.getAnnIds(imgIds=image_ids))
+        
+        for ann in annots:
+            if ann['category_id'] == cat_id and ann['bbox'][2] >= min_size and ann['bbox'][3] >= min_size:
+                flt_annots[cat].append({
+                    'image_id': ann['image_id'],
+                    'bbox': ann['bbox']
                 })
     
-    return filt_image_ids
+    cat_ids = dataset.getCatIds(catNms=categories)
+    other_cat_ids = [c for c in dataset.getCatIds() if c not in cat_ids]
+    
+    flt_annots['other'] = []
+    for cat_id in other_cat_ids:
+        image_ids = dataset.getImgIds(catIds=[cat_id])
+        for image_id in image_ids:
+            flt_annots['other'].append({
+                'image_id': image_id,
+                'bbox': None
+            })
+
+    return flt_annots
 
 
 def write_images(dataset, filt_img_ids):
     for cat in list(filt_img_ids.keys()):
         counter = 0
         save_path = os.path.join(args.data_path, '{}_crop'.format(args.version), cat)
+        if os.path.exists(save_path):
+            shutil.rmtree(save_path)
         os.makedirs(os.path.join(save_path), exist_ok=True)
         img_ids = filt_img_ids[cat]
         pbar = tqdm(total=len(img_ids), desc='Writing images for {}'.format(cat))
@@ -59,20 +66,19 @@ def write_images(dataset, filt_img_ids):
             img_info = dataset.loadImgs(img_id['image_id'])[0]
             img_path = os.path.join(args.data_path, args.version, img_info['file_name'])
             img = cv2.imread(img_path)
-            x, y, w, h = img_id['bbox']
-            x, y, w, h = int(x), int(y), int(w), int(h)
-            sub_img = img[y:y+h, x:x+w]
-            sub_img = cv2.resize(sub_img, (args.min_size, (args.min_size * sub_img.shape[0]) // sub_img.shape[1]))
-            cv2.imwrite(os.path.join(save_path, str(counter) + '.jpg'), sub_img)
+            if img_id['bbox'] is not None:
+                x, y, w, h = img_id['bbox']
+                x, y, w, h = int(x), int(y), int(w), int(h)
+                img = img[y:y+h, x:x+w]
+            img = cv2.resize(img, (args.min_size, (args.min_size * img.shape[0]) // img.shape[1]))
+            cv2.imwrite(os.path.join(save_path, str(counter) + '.jpg'), img)
             counter += 1
             pbar.update(1)
         pbar.close()
 
 
 if __name__ == "__main__":
-    categories = ["person", "car"]
+    categories = ["person", "car", "bicycle"]
     dataset = load_data(args)
-    filt_img_ids = filter_images(dataset, args.min_size, categories)
+    filt_img_ids = filter_annotations(dataset, args.min_size, categories)
     write_images(dataset, filt_img_ids)
-    
-    
---
+++ drivisafe/rec_cars_people/train.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+import torch
+from torchvision.datasets import ImageFolder
+from torch.utils.data import DataLoader
+from torchvision.transforms import transforms
+from torchvision.models import resnet50, ResNet50_Weights
+from tqdm import tqdm
+
+
+if __name__ == "__main__":
+    
